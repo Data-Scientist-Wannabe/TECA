8ff832db98a9ce78a3bba1ccbfe51d19f23ed9f6
Burlen Loring <bloring@lbl.gov>
2019-09-04 16:30:30 -0700

    look at status on docker

--- test/travis_ci/docker/ctest_linux.sh
+++ test/travis_ci/docker/ctest_linux.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -v
 export DASHROOT=`pwd`
-#export SHA=`git --git-dir=${TECA_DIR}/.git log --pretty=format:'%h' -n 1`
 export SHA=`git log --pretty=format:'%h' -n 1`
 if [[ "$DOCKER_IMAGE" == "fedora" ]]; then
     source /usr/share/Modules/init/bash
@@ -18,9 +17,9 @@ ctest -S ${DASHROOT}/test/travis_ci/docker/ctest_linux.cmake -V --timeout 180
 status=$?
 if [ ${status} -ne 0 ]
 then
-    echo "TESTS FAILED! $?" 
+    echo "TESTS FAILED! ${status}" 
     exit 1
 else
-    echo "TESTS PASSED! $?" 
+    echo "TESTS PASSED! ${status}" 
     exit 0
 fi
